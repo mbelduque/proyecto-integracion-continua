0f03cce11f08636d815a3b91813ab16b802dfa09
mbelduque <m.belduque@hotmail.com>
2020-06-01 00:28:35 -0500

    visual update

--- organic-shop/src/App.js
+++ organic-shop/src/App.js
@@ -36,7 +36,8 @@ class App extends Component {
       <div className="App" >
         <header className="App-header" >
           <img src={logo} className="App-logo" alt="logo" />
-          <p>¿necesita una cuenta? <hr /> Regístrese ahora mismo y haga sus compras</p>
+          <h1>Orgánica</h1>
+          <p id="txt">¿necesita una cuenta? <hr /> Regístrese ahora mismo y haga sus compras</p>
           {
             this.state.isSignedIn ? (
               <span>
